4e229ecc1bae4c5b9d8f9d67c1785594e84c5352
Ruben Nussbaumer <e235522@edu.sbl.ch>
2023-10-31 10:02:56 +0100

    Aufgabe 1 um kleines e zu löschen.

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+
+    if (currentElement === "e") {
+      result.push("")
+    } else {
+      result.push(currentElement)
+    }
+    
   }
   return result.join("")
 }
